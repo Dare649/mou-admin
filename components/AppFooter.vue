606e351bc5f64632341dbdf2b218f60b849ccfa4
Olaniran James <james@cygnusspin.com>
2020-05-05 18:43:49 +0100

    added method that returns copy rights year

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,7 +2,7 @@
     <div class=" container   container-fixed-lg footer">
         <div class="copyright sm-text-center text-center">
             <p class="small no-margin">
-                <span class="hint-text">Copyright &copy; <?php echo date('Y');?> </span>
+                <span class="hint-text">Copyright &copy; {{getCurrrentYear()}} </span>
                 <span class="font-montserrat">Michael Okpara University of Agriculture, Umudike</span>.
     <!--            <span class="hint-text">All rights reserved. </span>-->
             </p>
@@ -13,3 +13,13 @@
         </div>
     </div>
 </template>
+<script>
+export default {
+    methods: {
+        getCurrrentYear(){
+            var d = new Date();
+            return d.getFullYear();
+        }
+    }
+}
+</script>
