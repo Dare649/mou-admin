c078381e493b436c51037265783624ee5b0ab3fc
Chinedu Onwuasoanya <chinicerow@yahoo.com>
2020-09-23 09:09:55 +0100

    updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,4 @@
 const pkg = require('./package')
-import axios from 'axios';
-
-
-const dynamicRoutes = async () => {
-  const resForDepartments = await axios.get('https://portal.olsslekki.org/api/departments')
-  const resForLgas = await axios.get('https://portal.olsslekki.org/api/lgas')
-
-  const routesForDepartments = resForDepartments.data.map((department) => {
-    return {
-      route: `/get-started/departments/${department.id}`,
-      payload: department
-    }
-  })
-
-  const routesForLgas = resForLgas.data.map((lga) => {
-    return {
-      route: `/get-started/lgas/${lga.id}`,
-      payload: lga
-    }
-  })
-
-  return routesForDepartments.concat(routesForLgas)
-}
 
 module.exports = {
   mode: 'universal',
@@ -177,7 +154,6 @@ module.exports = {
     middleware: ['auth']
   },
   generate: {
-    routes: dynamicRoutes
   },
 
 
