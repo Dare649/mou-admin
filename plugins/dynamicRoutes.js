5e6b203f2475df81d076c28308e7ed73d6c630a5
Chinedu Onwuasoanya <chinicerow@yahoo.com>
2020-09-23 11:44:03 +0100

    dynamic route updates

--- plugins/dynamicRoutes.js
+++ plugins/dynamicRoutes.js
@@ -3,22 +3,29 @@ import axios from 'axios'
 export const dynamicRoutes = async () => {
 
   const resForDepartments = await axios.get('https://portal.olsslekki.org/api/faculties/getAllFaculties', {params: {size: 100}});
-  // console.log(resForDepartments);
+  const resForProgrammes = await axios.get('https://portal.olsslekki.org/api/departments/getAllDepartments');
   const resForLgas = await axios.get('https://portal.olsslekki.org/api/states/getAllStates', {params: {size: 100}});
 
-  const routesForDepartments = resForDepartments.data.data.map((department) => {
+  const routesForDepartments = resForDepartments.data.data.map((faculty) => {
     return {
-      route: `/get-started/departments/${department.id}`,
+      route: `/get-started/departments/${faculty.id}`,
+      payload: faculty
+    }
+  })
+
+  const routesForProgrammes = resForProgrammes.data.data.map((department) =>{
+    return {
+      route: `/get-started/programs/${department.id}`,
       payload: department
     }
   })
 
-  const routesForLgas = resForLgas.data.data.map((lga) => {
+  const routesForLgas = resForLgas.data.data.map((state) => {
     return {
-      route: `/get-started/lgas/${lga.id}`,
-      payload: lga
+      route: `/get-started/lgas/${state.id}`,
+      payload: state
     }
   })
 
-  return routesForDepartments.concat(routesForLgas)
+  return routesForDepartments.concat(routesForLgas, routesForProgrammes)
 }
