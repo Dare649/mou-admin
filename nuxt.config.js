fc1387366b0bb15054d8ab960cf5d712c3b237b6
elsopi <elsopi02@gmail.com>
2020-09-23 10:53:01 +0100

    dynamic routes update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,6 @@
+import {dynamicRoutes} from "./plugins/dynamicRoutes";
+
 const pkg = require('./package')
-import axios from 'axios';
-
-
-const dynamicRoutes = async () => {
-  const resForDepartments = await axios.get('https://portal.olsslekki.org/api/departments')
-  const resForLgas = await axios.get('https://portal.olsslekki.org/api/lgas')
-
-  const routesForDepartments = resForDepartments.data.map((department) => {
-    return {
-      route: `/get-started/departments/${department.id}`,
-      payload: department
-    }
-  })
-
-  const routesForLgas = resForLgas.data.map((lga) => {
-    return {
-      route: `/get-started/lgas/${lga.id}`,
-      payload: lga
-    }
-  })
-
-  return routesForDepartments.concat(routesForLgas)
-}
 
 module.exports = {
   mode: 'universal',
--- plugins/dynamicRoutes.js
+++ plugins/dynamicRoutes.js
@@ -1,17 +1,19 @@
 import axios from 'axios'
 
-const dynamicRoutes = async () => {
-  const resForDepartments = await axios.get('https://portal.olsslekki.org/api/departments')
-  const resForLgas = await axios.get('https://portal.olsslekki.org/api/lgas')
+export const dynamicRoutes = async () => {
 
-  const routesForDepartments = resForDepartments.data.map((department) => {
+  const resForDepartments = await axios.get('https://portal.olsslekki.org/api/departments');
+  // console.log(resForDepartments);
+  const resForLgas = await axios.get('https://portal.olsslekki.org/api/lgas');
+
+  const routesForDepartments = resForDepartments.data.data.data.map((department) => {
     return {
       route: `/get-started/departments/${department.id}`,
       payload: department
     }
   })
 
-  const routesForLgas = resForLgas.data.map((lga) => {
+  const routesForLgas = resForLgas.data.data.data.map((lga) => {
     return {
       route: `/get-started/lgas/${lga.id}`,
       payload: lga
