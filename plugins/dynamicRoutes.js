296f1c131963c9c6f68abfaae9e0c0a9d8bcc5a0
Chinedu Onwuasoanya <chinicerow@yahoo.com>
2020-09-23 11:16:57 +0100

    updates

--- plugins/dynamicRoutes.js
+++ plugins/dynamicRoutes.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 export const dynamicRoutes = async () => {
 
-  const resForDepartments = await axios.get('https://portal.olsslekki.org/api/departments', {params: {size: 100}});
+  const resForDepartments = await axios.get('https://portal.olsslekki.org/api/departments/getAllDepartments', {params: {size: 100}});
   // console.log(resForDepartments);
-  const resForLgas = await axios.get('https://portal.olsslekki.org/api/lgas', {params: {size: 100}});
+  const resForLgas = await axios.get('https://portal.olsslekki.org/api/lgas/getAllLgas', {params: {size: 100}});
 
   const routesForDepartments = resForDepartments.data.data.data.map((department) => {
     return {
