5ea73c0103f9630323d4d307de13eddc8978c887
Chinedu Rowland <chinicerow@yahoo.com>
2021-03-20 23:12:03 +0100

    *

--- pages/academic-session/student-acad-session/index.vue
+++ pages/academic-session/student-acad-session/index.vue
@@ -13,18 +13,53 @@
     <!-- END BREADCRUMBS -->
 
     <!-- ADD RECORD MODAL STARTS HERE -->
-    <div class="modal fade SlideUp" id="accommodation_modal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal fade SlideUp" id="session-modal" tabindex="-1" role="dialog" aria-hidden="true">
       <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
         <i class="pg-close"></i>
       </button>
       <div class="modal-dialog modal-lg">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="text-left p-b-5"><span class="semi-bold" id="title"></span></h5>
+            <h5 class="text-left p-b-5">
+              <span class="semi-bold" id="title"></span>
+            </h5>
           </div>
           <div class="modal-body">
-            <form @submit.prevent="">
-
+            <hr />
+            <form @submit.prevent="saveSession">
+              <div class="row">
+                <div class="col-lg-6 m-b-10">
+                  <input type="text" v-model="formData.session_name" placeholder="Session name" class="form-control" />
+                </div>
+                <div class="col-lg-6 m-b-10">
+                  <input type="text" v-model="formData.course_registration_fee" placeholder="Course Reg Fee" class="form-control" />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-6 m-b-10">
+                  <input type="text" v-model="formData.matriculation_year" placeholder="Matriculation Year" class="form-control" />
+                </div>
+                <div class="col-lg-6 m-b-10">
+                  <input type="text" v-model="formData.late_fees_amount" placeholder="Late Registration Fee" class="form-control" />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-6 m-b-10">
+                  <input type="date" v-model="formData.late_fees_start_date" class="form-control" />
+                </div>
+                <div class="col-lg-6 m-b-10">
+                  <input type="date" v-model="formData.late_fees_end_date" class="form-control" />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-12 m-b-10">
+                  <select class="form-control" v-model="formData.status">
+                    <option value="">-Status-</option>
+                    <option value="1">Active</option>
+                    <option value="0">Inactive</option>
+                  </select>
+                </div>
+              </div>
               <hr />
               <div class="row">
                 <div class="col-lg-12">
@@ -48,8 +83,12 @@
         <div class="card-header ">
           <h3 class="text-primary no-margin pull-left sm-pull-reset">Regular Session Setup</h3>
           <div class="pull-right sm-pull-reset">
-            <button type="button" @click="refresh()" class="btn btn-success btn-sm"><i class="fa fa-refresh"></i>&nbsp; Refresh </button>
-            <button type="button" class="btn btn-warning btn-sm"><i class="fa fa-plus"></i>&nbsp; Add New Record </button>
+            <button type="button" @click="refresh()" class="btn btn-success btn-sm">
+              <i class="fa fa-refresh"></i>&nbsp; Refresh
+            </button>
+            <button type="button" @click="openModal()" class="btn btn-warning btn-sm">
+              <i class="fa fa-plus"></i>&nbsp; Add New Record
+            </button>
           </div>
           <div class="clearfix"></div>
         </div>
@@ -79,8 +118,10 @@
                   <td v-if="session.status === 1">Active</td>
                   <td v-if="session.status === 0">Inactive</td>
                   <td>
-                    <span data-placement="top" data-toggle="tooltip" title="Edit Record">
-                      <button type="button" class="btn btn-default btn-sm" role="button"><i class="fa fa-pencil"></i></button>
+                    <span data-placement="top" @click="editSession(session)" data-toggle="tooltip" title="Edit Record">
+                      <button type="button" class="btn btn-default btn-sm" role="button">
+                        <i class="fa fa-pencil"></i>
+                      </button>
                     </span>
                     <span data-placement="top" data-toggle="tooltip" title="Delete Record">
                       <button type="button" class="btn btn-default btn-sm" role="button"><i class="fa fa-trash"></i></button>
@@ -107,6 +148,16 @@ export default {
   data: () => ({
     loading: true,
     sessions: [],
+    formData: {
+      session_name: '',
+      late_fees_start_date: '',
+      late_fees_end_date: '',
+      late_fees_amount: '',
+      course_registration_fee: '',
+      matriculation_year: '',
+      status: '',
+      id: ''
+    },
     pagination: {
       total: 0,
       per_page: 2,
@@ -132,6 +183,81 @@ export default {
           this.loading = false
           this.$toast.error('An error occurred')
         })
+    },
+    saveSession()  {
+      if(this.formData.id === '') {
+        this.createSession()
+      }else{
+        this.updateSession()
+      }
+    },
+    createSession() {
+      $('#submitBtn').attr('disabled', true).html('<i class="fa fa-save" /> &nbsp;&nbsp; Saving...')
+      this.$store.dispatch('student-acad-session/saveSession', this.formData)
+        .then(res =>{
+          $('#submitBtn').attr('disabled', false).html('<i class="fa fa-save" /> &nbsp;&nbsp; Save Record')
+          if(res.data.status) {
+            this.closeModal()
+            this.getAcademicSession(1)
+            this.$toast.success(res.data.message)
+            return
+          }
+          this.$toast.error(res.data.message)
+        }).catch(err =>{
+          $('#submitBtn').attr('disabled', false).html('<i class="fa fa-save" /> &nbsp;&nbsp; Save Record')
+          this.$toast.error('An error occurred')
+        })
+    },
+    updateSession() {
+      $('#submitBtn').attr('disabled', true).html('<i class="fa fa-save" /> &nbsp;&nbsp; Updating...')
+      this.$store.dispatch('student-acad-session/updateSession', this.formData)
+        .then(res =>{
+          if(res.data.status) {
+            this.closeModal()
+            this.getAcademicSession(1)
+            this.$toast.success(res.data.message)
+            return
+          }
+
+          this.$toast.error(res.data.message)
+        }).catch(err =>{
+          $('#submitBtn').attr('disabled', false).html('<i class="fa fa-save" /> &nbsp;&nbsp; Update Record')
+          this.$toast.error('An error occurred')
+        })
+    },
+    editSession(session) {
+      this.formData = {
+        session_name: session.session_name,
+        late_fees_start_date: this.$moment(session.late_fees_start_date).format('Y-MM-DD'),
+        late_fees_end_date: this.$moment(session.late_fees_start_date).format('Y-MM-DD'),
+        late_fees_amount: session.late_fees_amount,
+        course_registration_fee: session.course_registration_fee,
+        matriculation_year: session.matriculation_year,
+        status: session.status,
+        id: session.id
+      }
+      $('#title').html('Edit Session')
+      $('#submitBtn').attr('disabled', false).html('<i class="fa fa-save" /> &nbsp;&nbsp; Update Record')
+      $('#session-modal').modal()
+    },
+    openModal() {
+      $('#title').html('Create new session')
+      $('#submitBtn').attr('disabled', true).html('<i class="fa fa-save" /> &nbsp;&nbsp; Save Record')
+      $('#session-modal').modal()
+    },
+    closeModal() {
+      this.formData = {
+        session_name: '',
+        late_fees_start_date: '',
+        late_fees_end_date: '',
+        late_fees_amount: '',
+        course_registration_fee: '',
+        matriculation_year: '',
+        status: '',
+        id: ''
+      }
+      $('#title').html('')
+      $('#session-modal').modal('hide')
     },
     refresh() {
       this.loading = true
