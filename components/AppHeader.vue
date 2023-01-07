ccd2dbde127084127fd02e205dd1d27f01c000ce
Chinedu Onwuasoanya <chinicerow@yahoo.com>
2023-01-07 20:24:36 +0100

    Updates to change student levels

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -117,7 +117,7 @@
                           <li class="" v-permission="'View Import Putme Images'"><nuxt-link to="/utilities/import-putme-images">Import PUTME Images</nuxt-link></li>
                           <li class="" v-permission="'View Import Previous Transactions'"><nuxt-link to="/utilities/import-old-transactions">Import Previous Transactions</nuxt-link></li>
                           <li class="" v-permission="'View Import Old Level'"><nuxt-link to="/utilities/import-old-olevel">Import O'Level</nuxt-link></li>
-                          <li class=""><nuxt-link to="/utilities/change-student-levels">Change Student Levels</nuxt-link></li>
+                          <li class="" v-permission="'View Change Student Levels'"><nuxt-link to="/utilities/change-student-levels">Change Student Levels</nuxt-link></li>
                         </ul>
                       </li>
                       <li>
